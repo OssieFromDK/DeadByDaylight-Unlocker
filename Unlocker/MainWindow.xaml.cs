2fdcc076eedeae8ae57ec3338b6e157a8cadc281
OssieFromDK <50819244+OssieFromDK@users.noreply.github.com>
2023-12-05 19:44:54 +0100

    Update MainWindow.xaml.cs

--- Unlocker/MainWindow.xaml.cs
+++ Unlocker/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace FortniteBurger
         internal static Classes.Mods.ModManager ModManager = new Classes.Mods.ModManager();
 
         internal static string DBDVersion = "7.4.0";
-        internal static string CurrVersion = "3.7.0";
+        internal static string CurrVersion = "3.7.1";
         internal static string CurrentType = "Steam";
 
         internal bool InQueue = false;
